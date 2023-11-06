59908f9a460d9f65af7184dcb13fbc8ce19daec8
Raihan rahoof <108122077+raihan-rahoof@users.noreply.github.com>
2023-11-06 10:10:22 +0530

    Time: 853 ms (63.63%), Space: 27.2 MB (67.61%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -3,13 +3,15 @@ class Solution:
         if not prices:
             return 0
 
-        min_price = prices[0]
+        l,r = 0, 1
         max_profit = 0
 
-        for price in prices:
-            if price < min_price:
-                min_price = price
+        while r<len(prices):
+            if prices[l]<prices[r]:
+                profit = prices[r]-prices[l]
+                max_profit = max(max_profit,profit)
             else:
-                max_profit = max(max_profit, price - min_price)
-
+                l=r
+            r+=1
         return max_profit
+            
